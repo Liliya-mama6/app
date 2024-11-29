3130201c6ef4d15c96a69a97799931174f6f4b3b
admin <vozdvizhenka@mail.ru>
2024-11-29 21:10:44 +0500

    first commit

---
+++ app/routers/task.py
@@ -0,0 +1,45 @@
+from fastapi import APIRouter
+from app.backend.db import Base
+from sqlalchemy import Column, String, Integer, Boolean, ForeignKey
+from sqlalchemy.orm import relationship
+from app.models import User
+
+router=APIRouter(prefix='/task', tags=['task'])
+
+
+class Task(Base):
+    __tablename__ = 'tasks'
+    __table_args__ = {'extend_existing':True}
+    id = Column(Integer, primary_key=True, index=True)
+    title=Column(String)
+    content = Column(String)
+    priority = Column(Integer, default=0)
+    completed = Column(Boolean, default=False)
+    user_id = Column(Integer, ForeignKey('users.id'), index=True, nullable=False)
+    slag = Column(String, unique=True)
+    user = relationship('User', back_populates='tasks')
+
+
+
+@router.get('/')
+def all_tasks():
+    pass
+
+@router.get('/task_id')
+def task_by_id():
+    pass
+
+
+@router.post('/create')
+def create_task():
+    pass
+
+
+@router.put('/update')
+def update_task():
+    pass
+
+
+@router.delete('/delete')
+def delete_task():
+    pass
---
+++ app/routers/user.py
@@ -0,0 +1,45 @@
+from fastapi import APIRouter
+from app.backend.db import Base
+from sqlalchemy import Column, String, Integer, Boolean, ForeignKey
+from sqlalchemy.orm import relationship
+from app.models import Task
+
+
+router1=APIRouter(prefix='/user', tags=['user'])
+
+
+class User(Base):
+    __tablename__ = 'users'
+    __table_args__ = {'extend_existing':True}
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String)
+    firstname = Column(String)
+    lastname = Column(String)
+    age = Column(Integer)
+    slag = Column(String, unique=True)
+    tasks = relationship('Task', back_populates='user')
+
+
+@router1.get('/')
+def all_users():
+    pass
+
+
+@router1.get('/user_id')
+def user_by_id():
+    pass
+
+
+@router1.post('/create')
+def create_user():
+    pass
+
+
+@router1.put('/update')
+def update_user():
+    pass
+
+
+@router1.delete('/delete')
+def delete_user():
+    pass
